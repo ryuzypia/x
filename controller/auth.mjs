de5ff63768e1791ab964b1a48af38e117e9a01f2
류정원 <ryuzy@naver.com>
2025-04-28 15:42:57 +0900

    환경변수 추가

--- controller/auth.mjs
+++ controller/auth.mjs
@@ -1,10 +1,11 @@
 import * as authRepository from "../data/auth.mjs";
 import * as bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import { config } from "../config.mjs";
 
-const secretKey = "abcdefg1234%^&*";
-const bcryptSaltRounds = 10;
-const jwtExpiresInDays = "2d";
+const secretKey = config.jwt.secretKey;
+const bcryptSaltRounds = config.bcrypt.saltRounds;
+const jwtExpiresInDays = config.jwt.expiresInSec;
 
 async function createJwtToken(id) {
   return jwt.sign({ id }, secretKey, { expiresIn: jwtExpiresInDays });
