71bc2b8d6e72a3bf7fc0baf53d0d66dd2bb38ab1
류정원 <ryuzy@naver.com>
2025-05-12 11:34:55 +0900

    Mongoose 적용

--- db/database.mjs
+++ db/database.mjs
@@ -1,19 +1,21 @@
-import { config } from "../config.mjs";
-import MongoDb from "mongodb";
+/*
+  Mongoose
+  - MongoDB + Node.js용 ORM(Object-Relational Mapping)
+  - 스키마를 정의
+  - 입력,수정,조회,삭제 모두 안정적이고 코드를 간결하게 작성
+*/
 
-let db;
+import { config } from "../config.mjs";
+import Mongoose from "mongoose";
 
 export async function connectDB() {
-  return MongoDb.MongoClient.connect(config.db.host).then((client) => {
-    db = client.db();
-    // console.log(db);
+  return Mongoose.connect(config.db.host);
+}
+
+export function useVirtualId(schema) {
+  schema.virtual("id").get(function () {
+    return this._id.toString();
   });
-}
-
-export function getUsers() {
-  return db.collection("users");
-}
-
-export function getPosts() {
-  return db.collection("posts");
+  schema.set("toJSON", { virtual: true });
+  schema.set("toObject", { virtual: true });
 }
